9c5fc5687d0b31af38aac93a644c9c904ded7fac
zain-khalid <zainkhalid000@gmail.com>
2021-07-28 16:17:42 +0500

    change database to cloud database

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose")
-
-mongoose.connect('mongodb://localhost:27017/mobilityHub',{useNewUrlParser:true, useUnifiedTopology:true},
+mongoose.connect('mongodb+srv://zain:YrnT0taaSbbarCdv@mobilityhub.bbuyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology:true},
 err => {
     if(!err)
         console.log("DB connected");
     else
         console.log("DB Error "+JSON.stringify(err))
-})
+});
+
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://zain:<password>@mobilityhub.bbuyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
