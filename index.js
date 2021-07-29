47e8a841b02f731412a88c95250441c39d1ed314
zain-khalid <zainkhalid000@gmail.com>
2021-07-29 11:54:41 +0500

    Change port

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ var userRoutes = require('./controllers/userController')
 var app = express()
 app.use(cors())
 app.use(bodyParser.json())
-
-app.listen(4000, ()=>console.log("Server Started at : 4000"))
+const port = 8080;
+app.listen(port, ()=>console.log(`Server Started at ${port}`))
 app.use('/user', userRoutes)
