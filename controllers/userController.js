896a27e7647cc7a4d2d3a5380d6fe6e9a2d6b32e
Zain Khalid <osama.jamil@live.com>
2021-08-16 22:19:45 +0500

    image uploading testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,7 +44,7 @@ router.post('/', (req, res)=>{
 })
 
 router.post('/image', (req, res)=>{
-    res.send("all is well")
+    res.send("all is well", req.body.user)
     // newRecord.save((err, docs)=>{
     //     if(!err) res.send(docs)
     //     else res.send("error while saving user records "+ JSON.stringify(err, undefined, 2))
