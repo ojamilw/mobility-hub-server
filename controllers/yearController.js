0c084cea5466bbf605a0e564b4e89283bd3e8f47
Zain Khalid <osama.jamil@live.com>
2021-08-23 19:43:00 +0500

    Year controller

---
+++ controllers/yearController.js
@@ -0,0 +1,49 @@
+const express = require('express')
+var router = express.Router()
+var ObjectID = require('mongoose').Types.ObjectId
+
+var { yearModel } = require('../models/dbModels')
+
+router.get('/', (req, res)=>{
+    yearModel.find((err, docs)=> {
+        if(!err) res.send(docs)
+        else console.log("error while retrieving  all records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.post('/', (req, res)=>{
+    var newRecord = new yearModel({
+        name: req.body.name,
+    })
+
+    newRecord.save((err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while saving  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.put('/:id', (req, res)=>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id: "+req.params.id)
+
+    var updateRecord = {
+        name: req.body.name,
+    }
+
+    yearModel.findByIdAndUpdate(req.params.id, {$set: updateRecord}, (err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while updating  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.delete('/:id', (req, res)=>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id: "+req.params.id)
+
+    yearModel.findByIdAndRemove(req.params.id, (err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while removing  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+module.exports = router
