7f7d572c646565e30d10d60e04bf746deef1b226
Zain Khalid <osama.jamil@live.com>
2021-08-23 19:47:42 +0500

    testing

---
+++ controllers/modelController.js
@@ -0,0 +1,51 @@
+const express = require('express')
+var router = express.Router()
+var ObjectID = require('mongoose').Types.ObjectId
+
+var { modelModel } = require('../models/dbModels')
+
+router.get('/', (req, res)=>{
+    modelModel.find((err, docs)=> {
+        if(!err) res.send(docs)
+        else console.log("error while retrieving  all records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.post('/', (req, res)=>{
+    var newRecord = new modelModel({
+        name: req.body.name,
+        make: req.body.make,
+        year: req.body.year
+    })
+
+    newRecord.save((err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while saving  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.put('/:id', (req, res)=>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id: "+req.params.id)
+
+    var updateRecord = {
+        name: req.body.name,
+    }
+
+    modelModel.findByIdAndUpdate(req.params.id, {$set: updateRecord}, (err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while updating  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.delete('/:id', (req, res)=>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id: "+req.params.id)
+
+    modelModel.findByIdAndRemove(req.params.id, (err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while removing  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+module.exports = router
---
+++ controllers/versionController.js
@@ -0,0 +1,49 @@
+const express = require('express')
+var router = express.Router()
+var ObjectID = require('mongoose').Types.ObjectId
+
+var { versionModel } = require('../models/dbModels')
+
+router.get('/', (req, res)=>{
+    versionModel.find((err, docs)=> {
+        if(!err) res.send(docs)
+        else console.log("error while retrieving  all records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.post('/', (req, res)=>{
+    var newRecord = new versionModel({
+        name: req.body.name,
+    })
+
+    newRecord.save((err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while saving  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.put('/:id', (req, res)=>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id: "+req.params.id)
+
+    var updateRecord = {
+        name: req.body.name,
+    }
+
+    versionModel.findByIdAndUpdate(req.params.id, {$set: updateRecord}, (err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while updating  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.delete('/:id', (req, res)=>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id: "+req.params.id)
+
+    versionModel.findByIdAndRemove(req.params.id, (err, docs)=>{
+        if(!err) res.send(docs)
+        else console.log("error while removing  records "+ JSON.stringify(err, undefined, 2))
+    })
+})
+
+module.exports = router
