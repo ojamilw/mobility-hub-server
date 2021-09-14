dd6da31cae0c4381393aa7b33918737ad0847b12
Zain Khalid <osama.jamil@live.com>
2021-09-14 14:18:52 +0500

    make uploads folder as a public folder for quickly access images

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ var makeControllerRoutes = require('./controllers/makeController')
 var yearControllerRoutes = require('./controllers/yearController')
 var modelConrollerRoutes = require('./controllers/modelController')
 var versionControllerRoutes = require('./controllers/versionController')
+var mapControllerRoutes = require('./controllers/mapController')
 
 var app = express()
 
 app.use(cors())
-// app.use(express.json())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 const port = 8080
+
+app.use('/uploads', express.static('uploads'))
 
 app.use('/user', userRoutes)
 app.use('/service', serviceRoutes)
@@ -41,6 +43,8 @@ app.use('/make', makeControllerRoutes)
 app.use('/year', yearControllerRoutes)
 app.use('/model', modelConrollerRoutes)
 app.use('/version', versionControllerRoutes)
+
+app.use('/map', mapControllerRoutes)
 
 app.use('/parser', parserControllerRoutes)
 
